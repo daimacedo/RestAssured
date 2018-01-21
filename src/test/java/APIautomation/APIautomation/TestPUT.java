b3a4885426f3233a4c8d6cb3a27e098443524267
Daiane Macedo <daiane.macedo@hotmail.com>
2018-01-21 16:03:31 -0200

    Adding different assertions

--- src/test/java/APIautomation/APIautomation/TestPUT.java
+++ src/test/java/APIautomation/APIautomation/TestPUT.java
@@ -3,9 +3,13 @@ package APIautomation.APIautomation;
 import org.junit.Test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import junit.framework.Assert;
+
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 import APIautomation.utils.CreatePost;
+import static org.assertj.core.api.Assertions.*;
 
 public class TestPUT {
 
@@ -24,8 +28,25 @@ public class TestPUT {
 		.put("http://localhost:3000/posts/5")
 		.then()
 		.statusCode(200)
+		// assertion json path using hamcrest
 		.body("title", is("Macarronada da hora"))
 		.body("author", is("Daiane Macedo"));
+		
+		
+		ValidatableResponse vr = given()
+		.contentType(ContentType.JSON)
+		.body(put1)
+		.when()
+		.put("http://localhost:3000/posts/5")
+		.then()
+		.statusCode(200);
+		
+		// Assertion json path using Assert junit
+		String title = vr.extract().path("title");
+		System.out.println(title);
+		Assert.assertEquals(title, "Macarronada da hora");
+		
+		
 		
 	}
 	
